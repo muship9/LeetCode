09915bc1d9bd2580dcf610028af1136b28c51d32
shinp09 <57585657+shinp09@users.noreply.github.com>
2023-12-23 17:40:49 +0900

    feat: python

---
+++ python/704.binary-search.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) -1
+        while lo <= hi:
+            mid =(lo + hi)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return -1
+        
+# @lc code=end
+
