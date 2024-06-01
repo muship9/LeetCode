090912988146a5468cd36278f82833d9a9f33862
shinp09 <57585657+shinp09@users.noreply.github.com>
2024-06-01 12:02:14 +0900

    feat: 167

---
+++ python/leetcode/editor/en/167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,81 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-
+# decreasing order, find two numbers such that they add up to a specific target 
+# number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1
+#  < index2 <= numbers.length. 
+# 
+#  Return the indices of the two numbers, index1 and index2, added by one as an 
+# integer array [index1, index2] of length 2. 
+# 
+#  The tests are generated such that there is exactly one solution. You may not 
+# use the same element twice. 
+# 
+#  Your solution must use only constant extra space. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We 
+# return [1, 2].
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: numbers = [2,3,4], target = 6
+# Output: [1,3]
+# Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We 
+# return [1, 3].
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: numbers = [-1,0], target = -1
+# Output: [1,2]
+# Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We 
+# return [1, 2].
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  2 <= numbers.length <= 3 * 10⁴ 
+#  -1000 <= numbers[i] <= 1000 
+#  numbers is sorted in non-decreasing order. 
+#  -1000 <= target <= 1000 
+#  The tests are generated such that there is exactly one solution. 
+#  
+# 
+#  👍 11582 👎 1403
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        ns = [x for x in numbers if x <= target]
+        left = 0
+        right = len(ns) - 1
+
+        while left < right:
+            r = ns[left] + ns[right]
+            if r == target:
+                return [left, right]
+            if r > target:
+                right -= 1
+            elif r < target:
+                left += 1
+        return []
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+a = Solution()
+print(a.twoSum([2, 3, 7, 11, 15], 9))
+print(a.twoSum([2, 3, 4], 6))
+print(a.twoSum([-1, 0], -1))
+print(a.twoSum([2, 3], 1))
