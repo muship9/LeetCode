7f3049f4e23859c529c91ee3a6b23bbda7bafb01
muship9 <57585657+shinp09@users.noreply.github.com>
2025-02-05 09:33:33 +0900

    fix: 尺取り法で実装

--- go/leetcode/editor/en/125.valid-palindrome.go
+++ go/leetcode/editor/en/125.valid-palindrome.go
@@ -44,7 +44,11 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 // A phrase is a **palindrome** if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 // Given a string `s`, return `true` *if it is a **palindrome**, or* `false` *otherwise*.
@@ -64,6 +68,7 @@ func main() {
 		"hello",    // 非回文
 		"あいいあ",     // 回文 (日本語)
 		"パタトクカシーーシカクトタパ", // 回文 (カタカナ)
+		"A man, a plan, a canal: Panama",
 	}
 
 	for _, s := range testCases {
@@ -71,20 +76,61 @@ func main() {
 	}
 }
 
-func isPalindrome(s string) bool {
+func badIsPalindromeme(s string) bool {
 	a := make([]rune, 0, len(s))
 	b := make([]rune, 0, len(s))
 	for _, c := range s {
-		// 先順で文字列を格納
-		a = append(a, c)
+		if unicode.IsLetter(c) || unicode.IsDigit(c) {
+			lowC := unicode.ToLower(c)
+			// 先順で文字列を格納
+			a = append(a, lowC)
 
-		// b の先頭に文字列を追加していくことで後順になる
-		// append は第一引数に Slice 第二引数に文字列を受け取るので引数に合わせて型を変える必要がある
-		// append(c, b)はエラーになるため以下のように型に合わせて定義
-		b = append([]rune{c}, b...)
+			// b の先頭に文字列を追加していくことで後順になる
+			// append は第一引数に Slice 第二引数に文字列を受け取るので引数に合わせて型を変える必要がある
+			// append(c, b)はエラーになるため以下のように型に合わせて定義
+			b = append([]rune{lowC}, b...)
+
+		}
 	}
-
 	return string(a) == string(b)
 }
 
+func isPalindrome(s string) bool {
+	t := ""
+	for _, c := range s {
+		if unicode.IsLetter(c) || unicode.IsDigit(c) {
+			t += string(unicode.ToLower(c))
+		}
+	}
+
+	for i := 0; i < len(t)/2; i++ {
+		if t[i] != t[len(t)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
+
+// Solution に記載されてた一番早かったやつ
+func goodIsPalindrome(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+	s = strings.ToLower(s)
+	bytes := make([]byte, 0)
+	for _, r := range []byte(s) {
+		if (r >= 97 && r <= 122) || (r >= 48 && r <= 57) {
+			bytes = append(bytes, r)
+		}
+	}
+	for i, j := 0, len(bytes)-1; i < j; i++ {
+		if bytes[i] != bytes[j] {
+			fmt.Println(string(bytes[i]), string(bytes[j]))
+			return false
+		}
+		j--
+	}
+	return true
+}
